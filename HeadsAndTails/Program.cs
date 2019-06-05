b33118512d59ad5290d2de5011dc17b2980b4ce6
Your Name <gorosdotos@mail.ru>
2019-06-05 16:14:32 +0300

    refactoring + renaming + change reading prediction key

--- HeadsAndTails/Program.cs
+++ HeadsAndTails/Program.cs
@@ -12,36 +12,74 @@ namespace HeadsAndTails
     {
         static void Main(string[] args)
         {
-            ConsoleKeyInfo key;
-            int predictedResult;
-            int result;
+            ConsoleKeyInfo newGameKeyInfo;
+            ConsoleKeyInfo predictedResultKeyInfo;
+            int predictedResult = 0;
+            int result = 0;
             Random rand = new Random();
-            do
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Игра Орёл и Решка, от \"GOR\"! ");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Нажмите Enter для начала новой игры...");
+            Console.WriteLine();
+            newGameKeyInfo = Console.ReadKey();
+
+            while (newGameKeyInfo.Key == ConsoleKey.Enter)
             {
+                Console.WriteLine("Угадайте результат(нажмите соответствующую клавишу):");
+                Console.WriteLine("1 - Орел");
+                Console.WriteLine("2 - Решка");
+                Console.WriteLine();
+
+                do {
+                    predictedResultKeyInfo = Console.ReadKey();
+                    //print key
+                    Console.WriteLine();
+                    
+                    //break line
+                    Console.WriteLine();
+
+                    if (predictedResultKeyInfo.Key == ConsoleKey.D1)
+                    {
+                        predictedResult = 1;
+                    }
+                    else if (predictedResultKeyInfo.Key == ConsoleKey.D2)
+                    {
+                        predictedResult = 2;
+                    }
+                    else
+                    {
+                        predictedResult = -1;
+                        Console.WriteLine("Неверная клавиша. Повторите Ввод.");
+                        Console.WriteLine();
+                    }
+
+                } while (predictedResult == -1);
+
+                // get result
                 result = rand.Next(1, 3);
-                Console.WriteLine("Хотите сыграть в Орёл и Решку? Нажми Enter...");
-                key = Console.ReadKey();
-                Console.WriteLine("Угадай результат: нажми 1 если думаешь, что будет решка, 2 если будет орёл.");
-                predictedResult = int.Parse(Console.ReadLine());
-                for (int i = 0; i < 3; i++)
+
+                for (int i = 0; i < 10; i++)
                 {
-                    Console.WriteLine("...");
-                    Thread.Sleep(1000);
+                    Console.Write(".");
+                    Thread.Sleep(300);
                 }
 
-                if(result == predictedResult)
-                {
-                    Console.WriteLine("Победа");
-                }
-                else
-                {
-                    Console.WriteLine("Поражение");
-                }
-            }
-            while (key.Key == ConsoleKey.Enter);
-            {
+                Console.WriteLine();
+                Console.WriteLine();
 
+                string resultMessage = predictedResult == result ? "Ура, вы угадали!" : "Вы не угадали, попробуйте ещё!";
+                Console.WriteLine(resultMessage);
+
+                Console.WriteLine();
+                Console.WriteLine("Нажмите Enter для начала новой игры...");
+                Console.WriteLine();
+                newGameKeyInfo = Console.ReadKey();
             }
+            
         }
     }
 }

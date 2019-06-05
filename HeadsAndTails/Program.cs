fc996b5ccc96cb7dfcc9ff39406619e52034209f
Your Name <gorosdotos@mail.ru>
2019-06-05 14:57:56 +0300

    implementation of HeadsAndTails game

--- HeadsAndTails/Program.cs
+++ HeadsAndTails/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace HeadsAndTails
 {
@@ -10,6 +12,49 @@ namespace HeadsAndTails
     {
         static void Main(string[] args)
         {
+            ConsoleKeyInfo key;
+            int selection;
+            int result;
+            Random rand = new Random();
+            do
+            {
+                result = rand.Next(1, 3);
+                Console.WriteLine("Хотите сыграть в Орёл и Решку? Нажми Enter...");
+                key = Console.ReadKey();
+                Console.WriteLine("Угадай результат: нажми 1 если думаешь, что будет решка, 2 если будет орёл.");
+                selection = int.Parse(Console.ReadLine());
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("...");
+                    Thread.Sleep(1000);
+                }
+                if (result == 2)
+                {
+                    if (selection == 2)
+                    {
+                        Console.WriteLine("Победа");
+                    }
+                    else if (selection == 1)
+                    {
+                        Console.WriteLine("Поражение");
+                    }
+                }
+                else if (result < 2)
+                {
+                    if (selection == 1)
+                    {
+                        Console.WriteLine("Победа");
+                    }
+                    else if (selection == 2)
+                    {
+                        Console.WriteLine("Поражение");
+                    }
+                }
+            }
+            while (key.Key == ConsoleKey.Enter);
+            {
+
+            }
         }
     }
 }

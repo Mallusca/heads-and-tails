2d76c55f631f47d2f14489a7d1c232cb2cbe80be
Your Name <gorosdotos@mail.ru>
2019-06-05 15:19:56 +0300

    renamed selection=>predictedResult
    improve condition of win\lose

--- HeadsAndTails/Program.cs
+++ HeadsAndTails/Program.cs
@@ -13,7 +13,7 @@ namespace HeadsAndTails
         static void Main(string[] args)
         {
             ConsoleKeyInfo key;
-            int selection;
+            int predictedResult;
             int result;
             Random rand = new Random();
             do
@@ -22,33 +22,20 @@ namespace HeadsAndTails
                 Console.WriteLine("Хотите сыграть в Орёл и Решку? Нажми Enter...");
                 key = Console.ReadKey();
                 Console.WriteLine("Угадай результат: нажми 1 если думаешь, что будет решка, 2 если будет орёл.");
-                selection = int.Parse(Console.ReadLine());
+                predictedResult = int.Parse(Console.ReadLine());
                 for (int i = 0; i < 3; i++)
                 {
                     Console.WriteLine("...");
                     Thread.Sleep(1000);
                 }
-                if (result == 2)
+
+                if(result == predictedResult)
                 {
-                    if (selection == 2)
-                    {
-                        Console.WriteLine("Победа");
-                    }
-                    else if (selection == 1)
-                    {
-                        Console.WriteLine("Поражение");
-                    }
+                    Console.WriteLine("Победа");
                 }
-                else if (result < 2)
+                else
                 {
-                    if (selection == 1)
-                    {
-                        Console.WriteLine("Победа");
-                    }
-                    else if (selection == 2)
-                    {
-                        Console.WriteLine("Поражение");
-                    }
+                    Console.WriteLine("Поражение");
                 }
             }
             while (key.Key == ConsoleKey.Enter);
